d333c5f3d3cd492619fd050a08f934d90fe9720f
walte halm <walter_halm@hotmail.com>
2026-06-01 09:43:13 -0300

    feat: cambiar textos checkout - 'Ir a pagar' en carrito, 'Volver a la tienda' en pago

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,4 @@ from . import res_partner
 from . import res_users
 from . import res_config_settings
 from . import whatsapp_message
+from . import website
---
+++ models/website.py
@@ -0,0 +1,21 @@
+from odoo import models
+from odoo.tools.translate import _lt
+
+
+class Website(models.Model):
+    """Herencia de website para personalizar textos de botones del checkout."""
+    _inherit = 'website'
+
+    def _get_checkout_step_list(self):
+        """Sobreescribe textos de botones del checkout:
+        - Carrito: 'Checkout' → 'Ir a pagar'
+        - Pago: 'Back to delivery' → 'Volver a la tienda' (apunta a /shop)
+        """
+        steps = super()._get_checkout_step_list()
+        for xmlids, values in steps:
+            if 'website_sale.cart' in xmlids:
+                values['main_button'] = _lt("Ir a pagar")
+            if 'website_sale.payment' in xmlids:
+                values['back_button'] = _lt("Volver a la tienda")
+                values['back_button_href'] = '/shop'
+        return steps
